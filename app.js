193b3e5d478adcc9ec5d851a4d65004a132d36bd
Fernanda Moreno <fernandamoreno494@gmail.com>
2019-12-13 01:14:06 -0500

    Fix  app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const connectDB = require('./database/Connection');
 const app = express();
 
 /*Routes - Middleware*/
+const bandRoute = require('./routes/band');
+const authRoute = require('./routes/auth');
+const songRoute = require('./routes/song');
+const instrumentRoute = require('./routes/instrument');
+const resourceRoute = require('./routes/resources');
+const scenographyRoute = require('./routes/scenography');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use('/band', bandRoute);
@@ -12,12 +19,6 @@ app.use('/inst', instrumentRoute);
 app.use('/resource', resourceRoute);
 app.use('/scen', scenographyRoute);
 
-const bandRoute = require('./routes/band');
-const authRoute = require('./routes/auth');
-const songRoute = require('./routes/song');
-const instrumentRoute = require('./routes/instrument');
-const resourceRoute = require('./routes/resources');
-const scenographyRoute = require('./routes/scenography');
 
 /*Routes */
 app.get('/', (req, res) =>{
